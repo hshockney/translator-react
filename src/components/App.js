667cb27721fbbd84f5853aabf922341f32400ec6
hshockney <shockney@atlanticbb.net>
2019-07-08 14:37:25 -0400

    Start of app to pick a language and see the language change on screen

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+
+
+class App extends React.Component {
+    state = { language: 'english'};
+    onLanguageChange = language => {
+        this.setState({ language });
+    }
+    render() {
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('English')} />
+                    <i className="flag nl" onClick={() => this.onLanguageChange('Dutch')} />
+                </div>
+                    <UserCreate />
+            </div>
+        )
+    }
+}
+export default App;
